d4bf16b63ceaed18435743f1eab138aaa255340c
nohman20 <nohman20@gmail.com>
2016-07-17 13:05:19 -0700

    Fixes router config file

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,8 +1,10 @@
 import React from 'react'
-import { Route, IndexRoute } from 'react-router'
+import { Route, Router, IndexRoute } from 'react-router'
 import App from '../components/App'
 
 export default (
-<Route path="/" component={App}>
-</Route>
+<Router>
+  <Route path="/" component={App}>
+  </Route>
+</Router>
 )
