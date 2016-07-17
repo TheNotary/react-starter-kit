67b12ed7240100bb66f1547090eb8f22efe6ba88
nohman20 <nohman20@gmail.com>
2016-07-17 11:21:34 -0700

    Routes.jsx now connects App component to index route

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 var HtmlWebpackPlugin = require('html-webpack-plugin')
 var HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
-  template: __dirname + '/index.html',
+  template: __dirname + '/src/index.html',
   filename: 'index.html',
   inject: 'body'
 })
